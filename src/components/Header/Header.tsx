2eec8ba1afefde379c08e3aa20c7db88da8bd7ac
Natã Pedro <natansilva@plusoft.com>
2024-02-23 12:34:44 -0300

    feat: toggle to dark/light/system mode

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
+import { ModeToggle } from "../mode-toggle";
+
 const Header = () => {
     return (
         <header className=" flex h-16 w-3/4 items-center justify-between m-auto">
             <div className="logo text-2xl">pdro.</div>
             <nav>
-                <ul className="flex gap-8">
+                <ul className="flex gap-8 items-center ">
                     <li>Work</li>
                     <li>Projects</li>
                     <li>About me</li>
                     <li>Contact</li>
+                    <ModeToggle />
                 </ul>
             </nav>
         </header>
