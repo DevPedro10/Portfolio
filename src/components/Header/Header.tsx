38a6bc432698bf2d6c05a1619f82bc999f66da9e
Natã Pedro <natansilva@plusoft.com>
2024-02-23 17:21:39 -0300

    style: changing spacing header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,16 @@
 import { ModeToggle } from "../mode-toggle";
+import { Button } from "../ui/button";
 
 const Header = () => {
     return (
         <header className=" flex h-16 w-3/4 items-center justify-between m-auto">
             <div className="logo text-2xl">pdro.</div>
             <nav>
-                <ul className="flex gap-8 items-center ">
-                    <li>Work</li>
-                    <li>Projects</li>
-                    <li>About me</li>
-                    <li>Contact</li>
+                <ul className="flex gap-4 items-center ">
+                    <li><Button variant={"ghost"}>Work</Button></li>
+                    <li><Button variant={"ghost"}>Projects</Button></li>
+                    <li><Button variant={"ghost"}>About me</Button></li>
+                    <li><Button variant={"outline"}>Say Hi</Button></li>
                     <ModeToggle />
                 </ul>
             </nav>
