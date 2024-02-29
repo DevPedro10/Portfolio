728a41ef9521ce35d43d883cfabb8d1ca803fe2b
Natã Pedro <natanpedrodasilva@gmail.com>
2024-02-28 22:26:49 -0300

    style: remove toggle and dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { ThemeProvider } from "./components/theme-provider"
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <>
       <Header />
       <Main />
-    </ThemeProvider>
+    </>
   )
 }
 
