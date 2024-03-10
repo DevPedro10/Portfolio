db40be66daae52a55f3c39babbf04f49c8cd51be
Natã Pedro <natanpedrodasilva@gmail.com>
2024-03-10 20:41:46 -0300

    refactor: using tailwind styled component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,29 +1,38 @@
-import { Github, Linkedin, Mouse } from 'lucide-react'
-import './Main.scss'
-
+import tw from 'tailwind-styled-components'
 import { Button } from '../ui/button';
+import { Github, Linkedin, Mouse } from 'lucide-react'
+
+const Container = tw.main`w-3/5 mx-auto mt-40`;
+const HomeContainer = tw.div``;
+const Title = tw.h1`text-violet-500 text-xl mb-3 ml-1 font-bold`;
+const Subtitle = tw.h2`text-zinc-100 text-7xl mb-3 font-leo font-semibold`
+const Description = tw.h3`ml-2 text-zinc-200 text-3xl font-thin mt-6`;
+const SocialContainer = tw.div`flex items-center mt-8 ml-2 gap-3`;
+const ResumeButton = tw(Button)`px-2 py-5 px-5 py-5`;
+const SocialButton = tw(Button)`px-2 py-5`;
+const GithubButton = tw(Github)`text-violet-500`;
+const LinkedinButton = tw(Linkedin)`text-violet-500`;
+const Scroll = tw.div`mt-40 blink text-zinc-50 animate-blink`;
+const MouseIcon = tw(Mouse)`mx-auto w-[32px] h-[32px]`;
+const MouseText = tw.p`text-center mt-2 font-bold`;
 const Main = () => {
     return (
-        <main className='w-3/5 mx-auto mt-40'>
-            <div>
-                <p className='text-violet-500 text-xl mb-3 ml-1 font-bold'>HI, I'M NATÃ.</p>
-                <h1 className='text-zinc-100 text-7xl mb-3 font-leo font-semibold'>Crafting web solutions.</h1>
-                <h2 className='ml-2 text-zinc-200 text-3xl font-thin mt-6'>A Full Stack Developer specializing in TypeScript, React, and Java, with a focus on browser-based projects. Currently, I work at Plusoft.</h2>
-
-                <div className='flex items-center mt-8 ml-2 gap-3'>
-                    <Button variant={"outline"} className='px-10 py-5 text-zinc-50 hover:text-zinc-300'>Resume</Button>
-                    <Button className="px-2 py-5" variant={'link'}><Github className="text-violet-500" /></Button>
-                    <Button className="px-2 py-5" variant={'link'}><Linkedin className="text-violet-500" /></Button>
-                </div>
-            </div>
-
-            <div className='mt-40 blink text-zinc-50'>
-                <Mouse className='mx-auto ' size={32} />
-                <p className='text-center mt-2 font-bold'>Scroll</p>
-            </div>
-
-            {/* <Section/> */}
-        </main>
+        <Container>
+            <HomeContainer>
+                <Title>HI, I'M NATÃ.</Title>
+                <Subtitle>Crafting web solutions.</Subtitle>
+                <Description>A Full Stack Developer specializing in TypeScript, React, and Java, with a focus on browser-based projects. Currently, I work at Plusoft.</Description>
+                <SocialContainer>
+                    <ResumeButton variant={"outline"}>Resume</ResumeButton>
+                    <SocialButton variant={'link'}><GithubButton /></SocialButton>
+                    <SocialButton variant={'link'}><LinkedinButton /></SocialButton>
+                </SocialContainer>
+            </HomeContainer>
+            <Scroll>
+                <MouseIcon />
+                <MouseText>Scroll</MouseText>
+            </Scroll>
+        </Container>
     )
 }
 export default Main;
--- src/components/Main/Section.tsx
+++
@@ -1,8 +0,0 @@
-
-const Section = () => {
-    return (
-        <section className="flex gap-40">
-        </section>
-    )
-}
-export default Section;
