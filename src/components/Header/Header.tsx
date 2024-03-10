bd287e664c1f48c33687a4f746b0324b1f312d23
Natã Pedro <natanpedrodasilva@gmail.com>
2024-03-10 20:15:12 -0300

    refactor: using tailwind styled component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,35 @@
-// import { ModeToggle } from "../mode-toggle";
+import tw from "tailwind-styled-components";
 import { Link } from "react-router-dom";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 
+const Container = tw.header`text-zinc-300 flex h-16 w-3/5 items-center justify-between mx-auto mt-10`;
+const AvatarContainer = tw.div`flex items-center gap-4`;
+const NameAvatar = tw.h1`text-xl font-semibold`;
+const NavBar = tw.nav``;
+const List = tw.ul`flex gap-6 items-center text-xl `;
 
 
 const Header = () => {
     return (
-        <header className=" text-zinc-300 flex h-16 w-3/5 items-center justify-between mx-auto mt-10">
-            <div className="flex items-center gap-4">
+        <Container>
+            <AvatarContainer>
                 <Avatar>
                     <AvatarImage src="https://github.com/DevPedro10.png" alt="Natã Profile" />
                 </Avatar>
-                <h1 className="text-xl font-semibold">Natã Pedro</h1>
-            </div>
-            <nav>
-                <ul className="flex gap-6 items-center text-xl ">
+                <NameAvatar>Natã Pedro</NameAvatar>
+            </AvatarContainer>
+            <NavBar>
+                <List className="flex gap-6 items-center text-xl ">
                     <Link to="/experience">Experience</Link>
                     <Link to="/projects">Projects</Link>
                     <Link to="/articles">Articles</Link>
                     <Link to="/about">About me</Link>
-                    {/* <ModeToggle /> */}
-                </ul>
-            </nav>
+                </List>
+            </NavBar>
 
-            <Button className="text-zinc-50 hover:text-zinc-300" variant={"outline"}>Say Hi</Button>
-        </header>
+            <Button variant={"outline"}>Say Hi</Button>
+        </Container>
     )
 }
 export default Header;
