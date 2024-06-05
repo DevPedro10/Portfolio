2556247c8507146d8c7f5ebbb8b8a52c37f0f574
Natã <natanpedrodasilva@gmail.com>
2024-06-04 21:25:47 -0300

    style: improving the style of the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,25 @@
+import './Header.scss'
 import tw from "tailwind-styled-components";
-import { Link } from "react-router-dom";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
+import { Mail, MailOpen } from 'lucide-react';
+import { useState } from 'react';
 
 const Container = tw.header`text-zinc-300 flex h-16 w-3/5 items-center justify-between mx-auto mt-10`;
 const AvatarContainer = tw.div`flex items-center gap-4`;
 const NameAvatar = tw.h1`text-xl font-semibold`;
 const NavBar = tw.nav``;
-const List = tw.ul`flex gap-6 items-center text-xl `;
+// const List = tw.ul`flex gap-6 items-center text-xl `;
+const ListItem = tw.li``;
+const Ancor = tw.a`customLink`;
 
 interface HeaderProps {
     noPhoto?: boolean;
 }
 
 const Header = ({ noPhoto }: HeaderProps) => {
+    const [hover, setHover] = useState(false);
+
     return (
         <Container>
             <AvatarContainer>
@@ -25,16 +31,33 @@ const Header = ({ noPhoto }: HeaderProps) => {
                 <NameAvatar>Natã Pedro</NameAvatar>
             </AvatarContainer>
             <NavBar>
-                <List className="flex gap-6 items-center text-xl ">
+                <ul>
                     {/* <Link to="/experience">Experience</Link> */}
-                    <Link to="/">Home</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/articles">Articles</Link>
-                    <Link to="/about">About me</Link>
-                </List>
+
+                    <ListItem>
+                        <Ancor className='customLink' href="/">Home</Ancor>
+                    </ListItem>
+                    <ListItem>
+                        <Ancor className='customLink' href="/projects">Projects</Ancor>
+                    </ListItem>
+                    <ListItem>
+                        <Ancor className='customLink' href="/articles">Articles</Ancor>
+                    </ListItem>
+                    <ListItem>
+                        <Ancor className='customLink' href="/about">About me</Ancor>
+                    </ListItem>
+                </ul>
             </NavBar>
 
-            <Button variant={"outline"}>Say Hi</Button>
+            <ul>
+                <ListItem>
+                    <a href="#" className="customLink" id="sayHi"
+                        onMouseEnter={() => setHover(true)}
+                        onMouseLeave={() => setHover(false)}
+                    >
+                        {hover ? <MailOpen /> : <Mail />} Say Hi</a>
+                </ListItem>
+            </ul>
         </Container>
     )
 }
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,7 +1,5 @@
 import tw from "tailwind-styled-components";
 import ProjectCard from "./ProjectCard";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "../ui/carousel";
-
 const MainSection = tw.section`w-full md:w-3/5 mx-auto h-screen`
 const TextDiv = tw.div`md:px-0`;
 const Title = tw.h1`text-violet-500 text-1xl font-bold mt-5`;
@@ -15,85 +13,59 @@ const Projects = () => {
                     <Title>PROJECTS</Title>
                     <SubTitle className="">Here are some of the projects i've worked on</SubTitle>
                 </TextDiv>
-                <div className="shrink grow flex-wrap">
-                    <Carousel className="w-full max-w-6xl">
-                        <CarouselContent>
-                            <CarouselItem>
-                                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
-                                    <div className="relative group overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-transform duration-300 ease-in-out hover:-translate-y-2">
-                                        <ProjectCard
-                                            thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
-                                            title="Lorem ipsum dolor"
-                                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
-                                            tags={["Typescript", "Javascript"]}
-                                            repositoryLink="https://www.google.com"
-                                            liveDemoLink="https://www.google.com"
-                                        />
-                                    </div>
-                                    <div className="relative group overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-transform duration-300 ease-in-out hover:-translate-y-2">
-                                        <ProjectCard
-                                            thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
-                                            title="Lorem ipsum dolor"
-                                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
-                                            tags={["Typescript", "Javascript"]}
-                                            repositoryLink="https://www.google.com"
-                                            liveDemoLink="https://www.google.com"
-                                        />
-                                    </div>
-                                    <div className="relative group overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-transform duration-300 ease-in-out hover:-translate-y-2">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
+                    <ProjectCard
+                        thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
+                        title="Lorem ipsum dolor"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
+                        tags={["Typescript", "Javascript"]}
+                        repositoryLink="https://www.google.com"
+                        liveDemoLink="https://www.google.com"
+                    />
+                    <ProjectCard
+                        thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
+                        title="Lorem ipsum dolor"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
+                        tags={["Typescript", "Javascript"]}
+                        repositoryLink="https://www.google.com"
+                        liveDemoLink="https://www.google.com"
+                    />
 
-                                        <ProjectCard
-                                            thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
-                                            title="Lorem ipsum dolor"
-                                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
-                                            tags={["Typescript", "Javascript"]}
-                                            repositoryLink="https://www.google.com"
-                                            liveDemoLink="https://www.google.com"
-                                        />
-                                    </div>
-                                </div>
-                            </CarouselItem>
-                            <CarouselItem>
-                                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
-                                    <div className="relative group overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-transform duration-300 ease-in-out hover:-translate-y-2">
+                    <ProjectCard
+                        thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
+                        title="Lorem ipsum dolor"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
+                        tags={["Typescript", "Javascript"]}
+                        repositoryLink="https://www.google.com"
+                        liveDemoLink="https://www.google.com"
+                    />
 
-                                        <ProjectCard
-                                            thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
-                                            title="Lorem ipsum dolor"
-                                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
-                                            tags={["Typescript", "Javascript"]}
-                                            repositoryLink="https://www.google.com"
-                                            liveDemoLink="https://www.google.com"
-                                        />
-                                    </div>
-                                    <div className="relative group overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-transform duration-300 ease-in-out hover:-translate-y-2">
+                    <ProjectCard
+                        thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
+                        title="Lorem ipsum dolor"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
+                        tags={["Typescript", "Javascript"]}
+                        repositoryLink="https://www.google.com"
+                        liveDemoLink="https://www.google.com"
+                    />
 
-                                        <ProjectCard
-                                            thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
-                                            title="Lorem ipsum dolor"
-                                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
-                                            tags={["Typescript", "Javascript"]}
-                                            repositoryLink="https://www.google.com"
-                                            liveDemoLink="https://www.google.com"
-                                        />
-                                    </div>
-                                    <div className="relative group overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-transform duration-300 ease-in-out hover:-translate-y-2">
+                    <ProjectCard
+                        thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
+                        title="Lorem ipsum dolor"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
+                        tags={["Typescript", "Javascript"]}
+                        repositoryLink="https://www.google.com"
+                        liveDemoLink="https://www.google.com"
+                    />
 
-                                        <ProjectCard
-                                            thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
-                                            title="Lorem ipsum dolor"
-                                            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
-                                            tags={["Typescript", "Javascript"]}
-                                            repositoryLink="https://www.google.com"
-                                            liveDemoLink="https://www.google.com"
-                                        />
-                                    </div>
-                                </div>
-                            </CarouselItem>
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
+                    <ProjectCard
+                        thumbnail="https://i.pinimg.com/originals/2d/73/5d/2d735d98fe75dcd5ff7c21dd92860bf7.jpg"
+                        title="Lorem ipsum dolor"
+                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate debitis, libero id placeat eos Lorem ipsum dolor sit amet"
+                        tags={["Typescript", "Javascript"]}
+                        repositoryLink="https://www.google.com"
+                        liveDemoLink="https://www.google.com"
+                    />
                 </div>
             </MainSection>
         </>
