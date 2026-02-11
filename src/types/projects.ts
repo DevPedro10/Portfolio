da58f2ca7824832e85f32842d5d35eb56ead0249
Natã Pedro <natanpedrodasilva@gmail.com>
2026-02-10 22:59:22 -0300

    feat: add local data schemas and global type definitions

---
+++ src/types/projects.ts
@@ -0,0 +1,10 @@
+export interface ProjectInterface {
+  id: number;
+  title: string;
+  description: string;
+  technologies: string[];
+  image_url: string;
+  github: string;
+  live: string;
+  isHighlighted?: boolean;
+}
