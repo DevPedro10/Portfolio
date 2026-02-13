9a30546bda6e75cbd21bf2556be5c4be7038fd2c
Natã Pedro <natanpedrodasilva@gmail.com>
2026-02-13 14:15:56 -0300

    chore(Config): Add sourceserif font and update configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,6 +21,7 @@ export default {
 			fontFamily: {
 				inter: ['Inter', 'sans-serif'],
 				space: ['Space Grotesk', 'sans-serif'],
+				sourceserif: ["gf_Source_Serif_4 variant0", 'serif'],
 				dmserif: ['"DM Serif Text"', 'serif'],
 			},
 			colors: {
